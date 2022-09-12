4f96b5f7cd09b334cd4810def7dc4a2816243ba8
Cefas Sathler <cefasheli@gmail.com>
2022-09-12 19:52:45 -0300

    update

---
+++ templates/lifting.js
@@ -0,0 +1,6 @@
+let macroPoker = game.macros.getName("teste")
+let value={foo : "$350", bar: "$540", zip: "$400" }
+
+await macroPoker.setFlag("world", "poder", value)
+macroPoker.getFlag("world", "poder").foo
+//returns $350
